80bfef4d092979745cec313cc4acdb1f23e9d9cb
Richard Cotte <rcotte1582@gmail.com>
2021-01-19 16:10:14 +0100

    Ajout des commentaires

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,8 +16,8 @@ public class Main
     static boolean bouclierActif = true;
 
     /**
-     * Method that ask the player name and then initiate the hp and
-     * the shield rate of the player to their max value
+     * Méthode qui demande au joueur son nom et ensuite initialise ses points de vie et de bouclier a leur valeures
+     * maximales
      */
     static void initPersonnage()
     {
@@ -30,9 +30,9 @@ public class Main
     }
 
     /**
-     * Mothod that simulate a try with a percentage
-     * @param X percentage to success
-     * @return if success
+     * Méthode qui simule un essaie avec un pourcentage
+     * @param X chance de succès
+     * @return si il y a succès
      */
     static boolean hasard(double X)
     {
@@ -40,9 +40,9 @@ public class Main
     }
 
     /**
-     * method that return a short between 0 and a given number
-     * @param born last born
-     * @return a short between 0 and "born"
+     * Méthode qui retourne un short entre 0 et un nombre (born) donné
+     * @param born la borne
+     * @return un short entre 0 et la borne
      */
     static short nombreAuHasard(short born)
     {
@@ -50,9 +50,9 @@ public class Main
     }
 
     /**
-     * Method that calculate the damage that the player deal to the enemy
-     * @param pvEnnemi remaining enemy health point
-     * @return remaining enemy health point
+     * Methode qui calcule les dégats que le joueur a infligé a un énnemis
+     * @param pvEnnemi pv ennemi restant
+     * @return pv ennemi restant
      */
     static short attaqueJoueur(short pvEnnemi)
     {
@@ -65,7 +65,7 @@ public class Main
     }
 
     /**
-     * Method that show the remaining health point and shield rate of the player
+     * Méthode qui montre les pv et le bouclier restant du joueur
      */
     static void afficherPersonnage()
     {
@@ -80,16 +80,22 @@ public class Main
         }
     }
 
+    /**
+     * Méthode qui calcule les dégats qu'un ennemi inflige au joueur
+     */
     static void attaqueEnnemi()
     {
         short dgtEnnemi = nombreAuHasard(MAX_ATTAQUE_ENNEMI);
+        // si le joueur n'a plus de bouclier, en enlève des points de vie
         if (ptsBouclier == 0) {
             ptsDeVie = (short) (ptsBouclier - dgtEnnemi + ptsDeVie);
             System.out.println("L'" + Util.color("ennemi" , Color.YELLOW) + " attaque " +
                     Util.color(nomPersonnage , Color.GREEN) + " ! Il lui fait " + dgtEnnemi + " points de dommages ! " +
-                    Util.color(nomPersonnage , Color.GREEN) + " perd " + Util.color(dgtEnnemi , Color.RED) + " points " +
-                    "de vie !");
+                    Util.color(nomPersonnage , Color.GREEN) + " perd " + Util.color(dgtEnnemi , Color.RED) +
+                    " points " + "de vie !");
         }
+        // si l'ennemi inflige plus de dégats que le joueur n'a de bouclier, on enlève d'abord des points de bouclier
+        // et le reste de l'attaque vas enlevé des pv
         else if (ptsBouclier < dgtEnnemi) {
             ptsDeVie = (short) (ptsDeVie + (ptsBouclier - dgtEnnemi));
             System.out.println("L'" + Util.color("ennemi" , Color.YELLOW) + " attaque " +
@@ -99,6 +105,7 @@ public class Main
                     Util.color((short) ((ptsBouclier - dgtEnnemi)*-1) , Color.RED) + " points de vie !");
             ptsBouclier = 0;
         }
+        // si le joueur a un bouclier, le bouclier prend les dégats avant la vie
         else {
             ptsBouclier = (short) (ptsBouclier - dgtEnnemi);
             System.out.println("L'" + Util.color("ennemi" , Color.YELLOW) + " attaque " +
@@ -107,10 +114,18 @@ public class Main
         }
     }
 
+    /**
+     * Methode qui combine l'attaque de l'ennemie et l'attaque du joueur
+     * @param ptsEnnemi points de vite ennemi
+     * @param joueurJoue booléen disant si c'est au tour du joueur ou pas
+     * @return les pv de l'ennemi
+     */
     static short attaque(short ptsEnnemi,boolean joueurJoue)
     {
         if (ptsEnnemi > 0 && ptsDeVie > 0) {
             if (joueurJoue) {
+                // on propose au joueur de pouvoir réparer sont bouclier si il est désactivé, ce qui a 1/3 de chance
+                // d'aboutir.
                 if (!bouclierActif) {
                     System.out.println("Votre bouclier est cassé... Tapez 'Y' si vous tentez de le réparer, tapez" +
                             "autre chose si vous ne voulez pas essayer");
@@ -139,6 +154,7 @@ public class Main
             }
             else {
                 attaqueEnnemi();
+                // L'ennemi a une chance sur dix de désactiver le bouclier du joueur lors de son attaque
                 if (hasard(0.1)) {
                     bouclierActif = false;
                     ptsBouclier = 0;
@@ -149,6 +165,10 @@ public class Main
         return ptsEnnemi;
     }
 
+    /**
+     * Méthode qui retourne une liste contenant les pv de chaque ennemis dont leur nombre est donné par le joueur
+     * @return list contenant les pv des ennemis
+     */
     static short[] initEnnemis() {
         System.out.println("Combien souhaitez-vous combattre d'ennemis ?");
         Scanner scan = new Scanner(System.in);
@@ -167,13 +187,13 @@ public class Main
         initPersonnage();
         short[] list = initEnnemis();
         short i = 0;
+        // Le jeu continu tant qu'il y a des ennemis et que le joueur a des points de vie
         for(short element : list){
             boolean joueurJoue;
             joueurJoue = hasard(0.5);
             System.out.println("Combat avec un ennemi possédant " + element + " points de vie");
             afficherPersonnage();
             System.out.println(" vs ennemi (" + Util.color((short) Math.max(element, 0), Color.PURPLE) + ")");
-
             while (ptsDeVie > 0 && element > 0) {
                 element = attaque(element, joueurJoue);
                 joueurJoue = !joueurJoue;
@@ -181,34 +201,28 @@ public class Main
                 System.out.println(" vs " + Util.color("ennemi" , Color.YELLOW) + " (" +
                         Util.color((short) Math.max(element, 0), Color.PURPLE) + ")");
             }
-
             if (ptsDeVie <= 0) {
                 System.out.println(Util.color(nomPersonnage, Color.GREEN) + " est mort mais a tué " + nbEnnemisTues +
                         " ennemis");
                 break;
             }
-
             else {
                 nbEnnemisTues++;
                 i++;
-
                 if (i == (short) list.length) {
                     System.out.println(Util.color(nomPersonnage, Color.GREEN) + " a tué tous les ennemis !");
                     break;
                 }
-
                 System.out.println("L'ennemi est mort ! Au suivant !");
-
+                // Le bouclier se régénère après chaque ennemis tué
                 if (bouclierActif) {
                     System.out.println("Régénération du bouclier : " +
                             Util.color("+" + REGENERATION_BOUCLIER_PAR_TOUR, Color.BLUE));
                     ptsBouclier = (short) Math.min(PTS_BOUCLIER, ptsBouclier + REGENERATION_BOUCLIER_PAR_TOUR);
                 }
-
                 System.out.println("Saisisser S pour passer au combat suivant ou n'importe quoi d'autre pour fuir...");
                 Scanner scan = new Scanner(System.in);
                 String reponse = scan.nextLine();
-
                 if (!reponse.equals("S")) {
                     System.out.println("Vous avez tué " + nbEnnemisTues + " ennemis mais êtes partis lâchement avant" +
                             " la fin");
